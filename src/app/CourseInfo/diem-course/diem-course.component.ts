f1bfd355ef0019dd5ce261c137ad0b751d3789c2
tuandung6673 <tuandung6673@gmail.com>
2022-08-16 13:56:07 +0700

    Diem-Course

---
+++ src/app/CourseInfo/diem-course/diem-course.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diem-course',
+  templateUrl: './diem-course.component.html',
+  styleUrls: ['./diem-course.component.css']
+})
+export class DiemCourseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
