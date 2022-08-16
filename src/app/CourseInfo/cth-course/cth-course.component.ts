c4d68b55a20168cceba6206e4a9f989ab4c639e7
tuandung6673 <tuandung6673@gmail.com>
2022-08-16 13:56:33 +0700

    ChuongTrinhHoc-Course

---
+++ src/app/CourseInfo/cth-course/cth-course.component.ts
@@ -0,0 +1,32 @@
+import { CourseSchedule } from './../../../models/courseSchedule.model';
+import { ActivatedRoute, Params } from '@angular/router';
+import { ApiService } from 'src/services/api.service.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cth-course',
+  templateUrl: './cth-course.component.html',
+  styleUrls: ['./cth-course.component.css']
+})
+export class CthCourseComponent implements OnInit {
+
+  id : string
+  coursesSchedule: CourseSchedule[] = []
+
+  constructor(private apiService: ApiService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.id = params['id']
+    })
+    this.getCourseSchedule(this.id)
+  }
+
+  getCourseSchedule(courseId: string) {
+    this.apiService.getCourseSchedule(0, 1000, courseId).subscribe((responseData) => {
+      // console.log(responseData.data.data);
+      this.coursesSchedule = responseData.data.data
+    })
+  }
+
+}
