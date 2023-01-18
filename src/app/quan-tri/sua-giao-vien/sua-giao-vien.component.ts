61946583ae9e5b9b08190dab40533c493ac780b9
tuandung6673 <tuandung6673@gmail.com>
2023-01-18 12:59:07 +0700

    update

--- src/app/quan-tri/sua-giao-vien/sua-giao-vien.component.ts
+++ src/app/quan-tri/sua-giao-vien/sua-giao-vien.component.ts
@@ -12,6 +12,7 @@ import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 })
 export class SuaGiaoVienComponent implements OnInit {
 
+  defaultAvatar = 'https://st3.depositphotos.com/1767687/16607/v/450/depositphotos_166074422-stock-illustration-default-avatar-profile-icon-grey.jpg'
   Editor = ClassicEditor;
   id: string = ''
   editTeacher: Teacher = new Teacher()
--- src/app/quan-tri/sua-lop-hoc/sua-lop-hoc.component.ts
+++ src/app/quan-tri/sua-lop-hoc/sua-lop-hoc.component.ts
@@ -12,7 +12,8 @@ export class SuaLopHocComponent implements OnInit {
 
   id: string
   isLoading: boolean = false
-  editClassroom: Classroom = new Classroom()
+  editClassroom: Classroom = new Classroom();
+  defaultAvatar = "https://st3.depositphotos.com/1767687/16607/v/450/depositphotos_166074422-stock-illustration-default-avatar-profile-icon-grey.jpg"
 
   constructor(private apiService: ApiService, private router: Router, private route: ActivatedRoute) { }
 
