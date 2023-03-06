6e2b58a7e6afc404b95ab03cd4ddde4851a6af42
tuandung6673 <tuandung6673@gmail.com>
2023-03-06 18:42:01 +0700

    update

--- src/app/user/tong-quan/tong-quan-subject/tong-quan-subject.component.ts
+++ src/app/user/tong-quan/tong-quan-subject/tong-quan-subject.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-tong-quan-subject',
   templateUrl: './tong-quan-subject.component.html',
-  styleUrls: ['./tong-quan-subject.component.css']
+  styleUrls: ['./tong-quan-subject.component.scss']
 })
 export class TongQuanSubjectComponent implements OnInit {
 
