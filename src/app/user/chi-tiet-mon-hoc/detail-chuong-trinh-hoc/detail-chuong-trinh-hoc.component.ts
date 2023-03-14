5f4ec00210b61f611522eb32490b1bdcfcd6a053
tuandung6673 <tuandung6673@gmail.com>
2023-03-14 23:52:16 +0700

    update

--- src/app/user/chi-tiet-mon-hoc/detail-chuong-trinh-hoc/detail-chuong-trinh-hoc.component.ts
+++ src/app/user/chi-tiet-mon-hoc/detail-chuong-trinh-hoc/detail-chuong-trinh-hoc.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-detail-chuong-trinh-hoc',
   templateUrl: './detail-chuong-trinh-hoc.component.html',
-  styleUrls: ['./detail-chuong-trinh-hoc.component.css']
+  styleUrls: ['./detail-chuong-trinh-hoc.component.scss']
 })
 export class DetailChuongTrinhHocComponent implements OnInit {
 
