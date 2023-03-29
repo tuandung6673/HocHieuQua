5f0495af221ce30f1ba5fe3cb773a2f9471e85a6
tuandung6673 <tuandung6673@gmail.com>
2023-03-30 00:11:57 +0700

    update

--- src/app/quan-tri/test/sua-bai-kiem-tra/sua-bai-kiem-tra.component.ts
+++ src/app/quan-tri/test/sua-bai-kiem-tra/sua-bai-kiem-tra.component.ts
@@ -131,7 +131,7 @@ export class SuaBaiKiemTraComponent implements OnInit {
       commentConfiguration: JSON.stringify(this.commentConfiguration),
       testCategoryId: this.test.testCategoryCode,
       isShowInAbilityTest: this.test.isShowInAbilityTest ? 1 : 0,
-      deadlineDate : this.test.deadlineDate == "Invalid date" ? null : this.test.deadlineDate,
+      deadlineDate : this.test.deadlineDate == "Invalid date" ? null : moment(this.test.deadlineDate, 'DD/MM/YYYY k:mm').format('YYYY-MM-DD k:mm'),
       isAutoSendMail: this.test.isAutoSendMail ? 1 : 0,
       status: this.test.status ? 1 : 0,
       isFree: this.test.isFree ? 1 : 0,
