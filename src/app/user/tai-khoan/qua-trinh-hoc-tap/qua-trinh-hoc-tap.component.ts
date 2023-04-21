0e8bd780601dfed6391a52f3c12cc88be26479b3
tuandung6673 <tuandung6673@gmail.com>
2023-04-21 16:51:39 +0700

    update

--- src/app/user/tai-khoan/qua-trinh-hoc-tap/qua-trinh-hoc-tap.component.ts
+++ src/app/user/tai-khoan/qua-trinh-hoc-tap/qua-trinh-hoc-tap.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-qua-trinh-hoc-tap',
   templateUrl: './qua-trinh-hoc-tap.component.html',
-  styleUrls: ['./qua-trinh-hoc-tap.component.css']
+  styleUrls: ['./qua-trinh-hoc-tap.component.scss']
 })
 export class QuaTrinhHocTapComponent implements OnInit {
 
