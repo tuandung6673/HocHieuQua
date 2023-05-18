d08d47c308696a12eb6a8f783583e31c6549c8ad
tuandung6673 <tuandung6673@gmail.com>
2023-05-18 17:53:09 +0700

    update

--- src/app/common/detail-answer/child-answer-component.ts
+++ src/app/common/detail-answer/child-answer-component.ts
@@ -4,25 +4,50 @@ import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic'
   selector: 'app-nhieu-lua-chon',
   styleUrls: ['./child-answer.scss'],
   template: `
+  <div *ngFor="let answer of quizzConfigSets">
     <div class="child-answer" style="display: flex">
-      <input style="margin: 0" type="checkbox" id="answer.int_ord" (click)="chooseHanlder(this)"/>
+      <input style="margin: 0" type="checkbox" id="answer.int_ord" (click)="chooseHanlder(answer)"/>
       <div style="margin-left: 10px">{{answer.answer}}</div>
     </div>
+  </div>
   `
 })
 export class AppNhieuLuaChonComponent implements OnInit {
-  @Input() answer : any;
+  @Input() quizzConfigSets : any;
   @Input() quizz;
+  selectedAnswer : any[] = [];
+  countOfRightAnswer : number = 0;
   constructor() {
 
   }
 
   ngOnInit(): void {
+    console.log(this.quizzConfigSets);
+    this.quizzConfigSets.map(item => {
+      if(item.result) {
+        this.countOfRightAnswer = this.countOfRightAnswer + 1;
+      }
+    })
+    console.log(this.countOfRightAnswer);
     
   }
 
   chooseHanlder(item) {
-    
+    const index = this.selectedAnswer.indexOf(item);
+    if(index == -1) {
+      this.selectedAnswer.push(item);
+    } else {
+      this.selectedAnswer.splice(index, 1)
+    }
+    this.checkResult()
+  }
+
+  checkResult() {
+    if(this.selectedAnswer.every(item => item.result) && this.selectedAnswer.length == this.countOfRightAnswer) {
+      console.log(`Câu ${this.quizz.order + 1} chọn đúng`);
+    } else {
+      console.log(`Câu ${this.quizz.order + 1} chọn sai`);
+    }
   }
 }
 
@@ -30,14 +55,16 @@ export class AppNhieuLuaChonComponent implements OnInit {
   selector: 'app-mot-lua-chon',
   styleUrls: ['./child-answer.scss'],
   template: `
+  <div *ngFor="let answer of quizzConfigSets">
     <div class="child-answer" style="display: flex">
-      <input style="margin: 0" type="radio" id="answer.int_ord" [name]="quizz.id" (click)="chooseHandler(this)"/>
+      <input style="margin: 0" type="radio" id="answer.int_ord" [name]="quizz.id" (click)="chooseHandler(answer)"/>
       <span style="margin-left: 10px" [innerHTML]="answer.answer"></span>
     </div>
+  </div>
   `
 })
 export class AppMotLuaChonComponent implements OnInit {
-  @Input() answer : any;
+  @Input() quizzConfigSets : any;
   @Input() quizz;
   constructor() {
 
@@ -48,7 +75,7 @@ export class AppMotLuaChonComponent implements OnInit {
   }
 
   chooseHandler(item) {
-    if(item.answer.result) {
+    if(item.result) {
       console.log(`Câu ${this.quizz.order + 1} chọn đúng`);
     } else {
       console.log(`Câu ${this.quizz.order + 1} chọn sai`);
@@ -60,25 +87,38 @@ export class AppMotLuaChonComponent implements OnInit {
   selector: 'app-nhieu-trinh-tha-don-xuong',
   styleUrls: ['./child-answer.scss'],
   template: `
+  <div *ngFor="let answer of quizzConfigSets">
     <div class="child-answer">
       <p-dropdown [options]="answerOptions" placeholder="Chọn đáp án"></p-dropdown>
     </div>
+  </div>
   `
 })
 export class AppNhieuTrinhThaDonXuongComponent implements OnInit {
-  @Input() answer: any;
+  @Input() quizzConfigSets;
   answerOptions : any[] = [];
   constructor() {
     
   }
 
   ngOnInit(): void {
-    this.answerOptions = JSON.parse(this.answer.answer).data.map((item) => {
-      return {
-        label: item.answer,
-        value: item.id
-      }
-    });
+    // Lỗiiiii (bị trùng options)
+    this.quizzConfigSets.forEach(item => {
+      const answer = JSON.parse(item.answer);
+      this.answerOptions = answer.data.map(item2 => {
+        return {
+          label: item2. answer,
+          value: item2.id
+        }
+      })
+    })
+
+    // this.answerOptions = JSON.parse(this.quizzConfigSets.answer).data.map((item) => {
+    //   return {
+    //     label: item.answer,
+    //     value: item.id
+    //   }
+    // });
     
   }
 }
@@ -87,14 +127,16 @@ export class AppNhieuTrinhThaDonXuongComponent implements OnInit {
   selector: 'app-dung-sai',
   styleUrls: ['./child-answer.scss'],
   template: `
+  <div *ngFor="let answer of quizzConfigSets">
     <div class="child-answer" style="display: flex">
-      <input style="margin: 0" type="radio" id="answer.int_ord" [name]="quizz.id" (click)="chooseHandler(this)"/>
+      <input style="margin: 0" type="radio" id="answer.int_ord" [name]="quizz.id" (click)="chooseHandler(answer)"/>
       <span style="margin-left: 10px" [innerHTML]="answer.answer"></span>
     </div>
+  </div>
   `
 })
 export class AppDungSaiComponent implements OnInit {
-  @Input() answer;
+  @Input() quizzConfigSets;
   @Input() quizz;
   constructor() {
 
@@ -105,7 +147,7 @@ export class AppDungSaiComponent implements OnInit {
   }
 
   chooseHandler(item) {
-    if(item.answer.result) {
+    if(item.result) {
       console.log(`Câu ${this.quizz.order + 1} chọn đúng`);
     } else {
       console.log(`Câu ${this.quizz.order + 1} chọn sai`);
@@ -124,14 +166,14 @@ export class AppDungSaiComponent implements OnInit {
 })
 export class AppDienVaoChoTrongComponent implements OnInit {
   value1 : string; 
-  @Input() answer : any;
+  @Input() quizzConfigSets : any;
   @Input() quizz;
   ngOnInit(): void {
       
   }
 
   handlerAnswer(item) {
-    if(this.value1 == item.answer.answer) {
+    if(this.value1 == item.quizzConfigSets.answer) {
       console.log(`Câu ${this.quizz.order + 1} điền đúng`);
     } else {
       console.log(`Câu ${this.quizz.order + 1} điền sai`);
@@ -143,14 +185,16 @@ export class AppDienVaoChoTrongComponent implements OnInit {
   selector: 'app-dien-vao-nhieu-khoang-trong',
   styleUrls: ['./child-answer.scss'],
   template: `
-    <div class="child-answer">
-      <input type="text" pInputText [(ngModel)]="value1"> 
+    <div *ngFor="let answer of quizzConfigSets">
+      <div class="child-answer">
+        <input type="text" pInputText [(ngModel)]="value1"> 
+      </div>
     </div>
   `
 }) 
 export class AppDienVaoNhieuKhoangTrong implements OnInit {
   value1:string;
-  @Input() answer;
+  @Input() quizzConfigSets;
   ngOnInit(): void {
       
   }
@@ -160,28 +204,29 @@ export class AppDienVaoNhieuKhoangTrong implements OnInit {
   selector: 'app-phu-hop',
   styleUrls: ['./child-answer.scss'],
   template: `
-    <div class="child-answer" style="display: flex; justify-content: space-between; align-items: center">
-      <span>{{question.left}}</span>
+    <div *ngFor="let answer of quizzConfigSets" class="child-answer" style="display: flex; justify-content: space-between; align-items: center">
+      <span>{{answer.answer.left}}</span>
       <span>
-        <p-dropdown [options]="this.questionOptions" [(ngModel)]="question.right" [placeholder]="question.left"></p-dropdown>
+        <p-dropdown [options]="questionOptions" [(ngModel)]="answer.answer.right" [placeholder]="answer.answer.left"></p-dropdown>
       </span>
     </div>
   `
 })
 export class AppPhuHopComponent implements OnInit {
-  @Input() answer : any;
   @Input() quizzConfigSets : any;
-  question : any;
   questionOptions : any[] = []
-  ngOnInit(): void {
+  constructor() {}
+  ngOnInit(): void { 
     this.quizzConfigSets.map(item => {
-      const answer = JSON.parse(item.answer);
-      this.questionOptions.push({
-        label: answer.right,
-        value: answer.right
-      })  
+      item.answer = JSON.parse(item.answer)
     })
-    this.question = JSON.parse(this.answer.answer);
+
+    this.quizzConfigSets.map(item => {
+      this.questionOptions.push({
+        label: item.answer.right,
+        value: item.answer.right
+      })
+    })
   }
 }
 
@@ -195,7 +240,7 @@ export class AppPhuHopComponent implements OnInit {
   `
 })
 export class AppTieuLuanComponent implements OnInit {
-  @Input() answer;
+  @Input() quizzConfigSets;
   Editor = ClassicEditor;
   content : string;
   ngOnInit(): void {
