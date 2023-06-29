2c21bb630ff7a7aecaa3ac77b517c5bdc5549c38
tuandung6673 <tuandung6673@gmail.com>
2023-06-29 22:56:21 +0700

    update

--- src/app/quan-tri/test/sua-bai-kiem-tra/sua-kt-cau-hoi/sua-kt-cau-hoi.component.ts
+++ src/app/quan-tri/test/sua-bai-kiem-tra/sua-kt-cau-hoi/sua-kt-cau-hoi.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Test } from 'src/models/test.model';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
   selector: 'app-sua-kt-cau-hoi',
@@ -8,6 +9,16 @@ import { Test } from 'src/models/test.model';
 })
 export class SuaKtCauHoiComponent implements OnInit, OnChanges {
   @Input() test : Test;
+  isEdit : boolean = false;
+  Editor = ClassicEditor;
+  testOptions = [
+    {label: 'Level 1', value: 1},
+    {label: 'Level 2', value: 2},
+    {label: 'Level 3', value: 3},
+    {label: 'Level 4', value: 4},
+    {label: 'Level 5', value: 5},
+  ];
+  quizzEdit : string;
   constructor() { }
 
   ngOnInit(): void {
@@ -16,7 +27,12 @@ export class SuaKtCauHoiComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    console.log(this.test);
+    // console.log(this.test);
+  }
+
+  editQuizz(quizzId) {
+    this.quizzEdit = quizzId;
+    this.isEdit = true;
   }
 
 }
