ccbb0ee54b82a7dceeac8001ce98a8b203549d21
Nguyen Tuan Dung <dungnt@unicloud.com.vn>
2023-07-02 11:40:48 +0700

    update

--- src/app/quan-tri/test/sua-bai-kiem-tra/sua-kt-cau-hoi/sua-kt-cau-hoi.component.ts
+++ src/app/quan-tri/test/sua-bai-kiem-tra/sua-kt-cau-hoi/sua-kt-cau-hoi.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Test } from 'src/models/test.model';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { ApiService } from 'src/services/api.service.service';
+import * as queryString from 'querystring-es3';
 
 @Component({
   selector: 'app-sua-kt-cau-hoi',
@@ -11,28 +13,79 @@ export class SuaKtCauHoiComponent implements OnInit, OnChanges {
   @Input() test : Test;
   isEdit : boolean = false;
   Editor = ClassicEditor;
-  testOptions = [
-    {label: 'Level 1', value: 1},
-    {label: 'Level 2', value: 2},
-    {label: 'Level 3', value: 3},
-    {label: 'Level 4', value: 4},
-    {label: 'Level 5', value: 5},
+  levelOptions = [
+    {label: 'Dễ', value: 1},
+    {label: 'Trung bình', value: 2},
+    {label: 'Khó', value: 3},
+    {label: 'Rất khó', value: 4},
   ];
+  questionTypeOptions : any[] = [];
+  questionGroupOptions : any[] = [];
+  teacherOptions : any[] = [];
+
   quizzEdit : string;
-  constructor() { }
+  constructor(
+    private apiService: ApiService,
+  ) { }
 
   ngOnInit(): void {
+    this.getQuestionType();
+    this.getQuestionGroup();
+    this.getTeacher();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
     console.log(this.test);
     
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    // console.log(this.test);
+  editQuizz(quizz) {
+    this.quizzEdit = quizz.id;
+    this.isEdit = true;
+    console.log(JSON.parse(quizz.teacherIds));
+    
   }
 
-  editQuizz(quizzId) {
-    this.quizzEdit = quizzId;
-    this.isEdit = true;
+  getQuestionType() {
+    const query = queryString.stringify({filter: '', offSet: 0, pageSize: 1000})
+    this.apiService.getTestQuestionType(query).subscribe(response => {
+      if(response.status === 'success') {
+        this.questionTypeOptions = response.data.data.map(item => {
+          return {
+            label: item.name,
+            value: item.code
+          }
+        })
+      }
+    })
+  }
+
+  getQuestionGroup() {
+    const query = queryString.stringify({filter: '', offSet: 0, pageSize: 1000, status: 1});
+    this.apiService.getTestQuestionGroup(query).subscribe(response => {
+      if(response.status === 'success') {
+        this.questionGroupOptions = response.data.data.map(item => {
+          return {
+            label: item.name,
+            value: item.id
+          }
+        })
+      }
+    })
+  }
+
+  getTeacher() {
+    const queryParams = queryString.stringify({filter: '', offSet: 0, pageSize: 1000})
+    this.apiService.getTeacher().subscribe(response => {
+      if(response.status === 'success') {
+        this.teacherOptions = response.data.data.map(item => {
+          return {
+            label: item.name,
+            value: item.id
+          }
+        })
+      }
+    })
   }
 
 }
