ee104184c9c47e9f97abf8ef74bc1bd13d756cdc
Nguyen Tuan Dung <dungnt@unicloud.com.vn>
2023-07-13 22:26:21 +0700

    update

---
+++ src/models/testQuestionAnswer.model.ts
@@ -0,0 +1,14 @@
+export class TestQuestionAnswer {
+    id: string = "";
+    questionId: string = "";
+    isCorrect: any = 0;
+    isSpecial: any = 0;
+    comment: string = "";
+    answer: string = "";
+    isParent: any = 0;
+    parentId: any = null;
+    answerLeft: string = "";
+    answerRight: string = "";
+    order: any = 0;
+    childQuestionAnswers: any = null
+}
