3eee84fef49bdbaa89c5aa5d57a866d2264429f2
Nguyen Tuan Dung <dungnt@unicloud.com.vn>
2023-09-25 17:18:56 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -282,6 +282,8 @@ import { ChiTietNamHocComponent } from './quan-tri/nam-hoc/chi-tiet-nam-hoc/chi-
     KtDungSaiComponent,
     KtDienVaoChoTrongComponent,
     KtPhuHopComponent,
+    NamHocComponent,
+    ChiTietNamHocComponent
       ],
   imports: [
     MathjaxModule.forRoot(),
